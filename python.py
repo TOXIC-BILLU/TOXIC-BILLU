02d71bee1843c15466399a2eb7a213d28ad610c7
toxic billu <soniaaktersoni44@gmail.com>
2024-05-27 15:18:00 +0600

    Add files via upload

---
+++ python.py
@@ -0,0 +1,8 @@
+   username = "python"
+    password = "1234"
+
+givenUsername = input("Enter your username")
+ if givenusername == username: 
+     print("correct usernamr")
+     else:
+         print("wrong user")
